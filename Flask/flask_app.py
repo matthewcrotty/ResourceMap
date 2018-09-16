07234cb266a776f7a3fc8c9cd504633c7005c563
Matthew Crotty <mjc2011@verizon.net>
2018-09-15 22:07:27 -0400

    Geoquery API calls using coordinates

--- Flask/flask_app.py
+++ Flask/flask_app.py
@@ -8,6 +8,7 @@ from fips import *
 
 app = Flask(__name__, template_folder='templates')
 
+censusKey = "1dc5156c4a13a23278eb8105175e8d9725fb69f0"
 
 @app.route("/")
 def index():
@@ -24,7 +25,7 @@ def index():
 @app.route("/popquery")
 def query():
     state = request.args.get('state')
-    state = getFIPSCodeState(state)
+    state = getFIPSCodeState('\"'+state+'\"')
     if not state:
         return render_template('error.html')
     request_url = "http://api.census.gov/data/2010/sf1?key="+censusKey+"&get=P0010001&for=state:"+state
@@ -33,12 +34,23 @@ def query():
     return text
 
 
-@app.route("/geoquery")
+@app.route("/geoquery", methods=('GET', 'POST'))
 def geoquery():
     lat = request.args.get('lat')
     lang = request.args.get('lang')
     request_url = "https://maps.googleapis.com/maps/api/geocode/json?&latlng="+lat+","+lang+"&key=AIzaSyBbbvgJb9Yc-qHEZYmcot9C3ZdTaWWsLjc"
     r = urllib.request.urlopen(request_url)
     data = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
-    return jsonify(data)
+    results = data['results']
+    state = ""
+    county = ""
+    for x in results:
+        for y in x['address_components']:
+            if y['types'][0] == 'administrative_area_level_2':
+                county = y['long_name']
+            elif y['types'][0] == 'administrative_area_level_1':
+                state = y['long_name']
+    print(county + ", " + state)
+    return redirect('/popquery?state='+state)
+
 
